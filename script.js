8205b4bbf16b4f0754890d3d947cd1257ad8458f
yehoshua-001 <joshuafelicilda248@gmail.com>
2026-08-27 20:19:35 +0800

    Edit script.js
    Added an array for books, object constructor for books, and another
    function for creating book from constructor then store (push) it to the
    array.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(`Initial commit; testing connections...`);
+const bookShelf = [];
+function Book(title, author, pages, datePublish, readStatus){
+    if(!new.target){
+        throw Error(`You must use the 'new' operator to call the constructor`);
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.datePublish = datePublish;
+    this.readStatus = readStatus;
+}
+function addBookToShelf(title, author, pages, datePublish, readStatus){
+    const book = new Book(title, author, pages, datePublish, readStatus);
+    bookShelf.push(book);
+}
+
+// Test (check console output)
+addBookToShelf(`book1`, `joshua`, `67`, `1738`, true);
+addBookToShelf(`book2`, `joshua`, `69`, `2026`, false);
+bookShelf.forEach(book => console.table(book));
